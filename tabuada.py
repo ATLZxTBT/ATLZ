6b0f91f0f65df51adecaedc89ff576c871d8a2e0
O Sr. LE BOOM <skywalkerfelipe0@gmail.com>
2026-04-14 14:08:36 -0300

    Add files via upload

---
+++ tabuada.py
@@ -0,0 +1,11 @@
+import time
+num = int(input(f"\n\033[3;32mDigite um número: \033[0m\n"))
+def timer(txt):
+    print(txt, end=" ", flush=True)
+    time.sleep(2)
+    print("  "*len(txt), end="\r", flush=True)
+timer(f"\033[1mCarregando...\033[0m")
+print(f"\033[1;32mTabuada do {num}:\033[0m\n")
+for i in range(10):
+    print(f"[{num} x {i+1} = {num * (i+1)}]") 
+    time.sleep(0.05)

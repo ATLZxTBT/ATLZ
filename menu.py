7f971332bbcdfebbc568e0141a0d7dfcea329937
O Sr. LE BOOM <skywalkerfelipe0@gmail.com>
2026-04-14 16:06:47 -0300

    Update menu.py

--- menu.py
+++ menu.py
@@ -1,3 +1,4 @@
+
 import sqlite3
 
 conn = sqlite3.connect("BANCO DE DADOS")
@@ -13,27 +14,28 @@ cursor.execute("""
 )
 
 print(f"\n\033[2m--==|[ NOVO ALUNO ]|==--\033[0m\n")
-novo_aluno = input(f"\033[1mNome do novo aluno: \033[0m")
-# Repete apenas a nota if nota == wrong
+novo_aluno = str(input(f"\033[1mNome do novo aluno: \033[0m"))
 while True:
     try:
         nota_novo_aluno = int(input(f"\033[1mNota do novo aluno: \033[0m"))
         break
     except ValueError:
         print(f"\n\033[2;32M !DIGITE UM NÚMERO VÁLIDO! \033[32m\n")
-        continue
 
 cursor.execute("INSERT INTO alunos (nome, nota) VALUES (?, ?)", (novo_aluno, nota_novo_aluno))
 
 conn.commit()
 
-print(f"--==|[{novo_aluno} com nota {nota_novo_aluno} foi \033[31madicionado\033[31m no BANDO DE DADOS]|==--\n")
+print(f"--==|[{novo_aluno} com nota [{nota_novo_aluno}] foi \033[32madicionado\033[0m no BANDO DE DADOS]|==--\n")
 
-print("=== dados tabela === ")
 cursor.execute("SELECT * FROM alunos")
 alunos = cursor.fetchall()
 
+
+print("-"*30)
+
+
 for aluno in alunos:
-    print(aluno)
+        print(aluno)
 
 conn.close()

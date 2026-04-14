6459430ede4de8f736831678df52d6f677759861
O Sr. LE BOOM <skywalkerfelipe0@gmail.com>
2026-04-14 15:10:57 -0300

    Add files via upload

---
+++ menu.py
@@ -0,0 +1,39 @@
+import sqlite3
+
+conn = sqlite3.connect("BANCO DE DADOS")
+cursor = conn.cursor()
+
+
+cursor.execute("""
+    CREATE TABLE IF NOT EXISTS alunos (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        nome TEXT NOT NULL,
+        nota INTEGER
+    )"""
+)
+
+print(f"\n\033[2m--==|[ NOVO ALUNO ]|==--\033[0m\n")
+novo_aluno = input(f"\033[1mNome do novo aluno: \033[0m")
+# Repete apenas a nota if nota == wrong
+while True:
+    try:
+        nota_novo_aluno = int(input(f"\033[1mNota do novo aluno: \033[0m"))
+        break
+    except ValueError:
+        print(f"\n\033[2;32M !DIGITE UM NÚMERO VÁLIDO! \033[32m\n")
+        continue
+
+cursor.execute("INSERT INTO alunos (nome, nota) VALUES (?, ?)", (novo_aluno, nota_novo_aluno))
+
+conn.commit()
+
+print(f"--==|[{novo_aluno} com nota {nota_novo_aluno} foi \033[31madicionado\033[31m no BANDO DE DADOS]|==--\n")
+
+print("=== dados tabela === ")
+cursor.execute("SELECT * FROM alunos")
+alunos = cursor.fetchall()
+
+for aluno in alunos:
+    print(aluno)
+
+conn.close()

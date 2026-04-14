54cc1a320691ec2a4159a23a0c6903cea78a5fff
O Sr. LE BOOM <skywalkerfelipe0@gmail.com>
2026-04-14 16:45:38 -0300

    Update menu.py

--- menu.py
+++ menu.py
@@ -1,4 +1,3 @@
-import sqlite3
 conn = sqlite3.connect("BANCO_DE_DADOS.db")
 cursor = conn.cursor()
 cursor.execute("""
@@ -8,13 +7,19 @@ cursor.execute("""
         nota INTEGER
     )""")
 print(f"\n\033[2m--==|[ NOVO ALUNO ]|==--\033[0m\n")
-novo_aluno = str(input(f"\033[1mNome do novo aluno: \033[0m"))
+while True:
+    novo_aluno = str(input(f"\033[1mNome do novo aluno (Máx. 20 Caractéres): \033[0m"))
+    if len(novo_aluno) > 20:
+        print(f"\n\033[2;31M !CARACTÉRES EXCEDIDOS (Tente abreviações)! \033[31m\n")
+        continue
+    else:
+        break
 while True:
     try:
         nota_novo_aluno = int(input(f"\033[1mNota do novo aluno: \033[0m"))
         break
     except ValueError:
-        print(f"\n\033[2;32M !DIGITE UM NÚMERO VÁLIDO! \033[32m\n")
+        print(f"\n\033[2;31M !DIGITE UM NÚMERO VÁLIDO! \033[31m\n")
 cursor.execute("INSERT INTO alunos (nome, nota) VALUES (?, ?)", (novo_aluno, nota_novo_aluno))
 conn.commit()
 print(f"--==| \033[32m{novo_aluno}\033[0m com nota [{nota_novo_aluno}] foi \033[32madicionado\033[0m no BANDO DE DADOS |==--\n")
